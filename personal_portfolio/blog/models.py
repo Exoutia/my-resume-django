01dbe2bb83499551b0453956217db6165a5a398e
Exoutia <jhav365@gmail.com>
2023-03-13 19:06:50 +0530

    created the model for the blogs app with post, comment, category

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # delete on cascade will delete all the comments
+    # related to the post when post is deleted.
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
