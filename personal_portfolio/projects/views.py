191c340bfa4a8a8f087c5cbae3808094b8bed6a4
Exoutia <jhav365@gmail.com>
2023-03-13 10:22:26 +0530

    adding the project index in view of projects to show the projects in site.

--- personal_portfolio/projects/views.py
+++ personal_portfolio/projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from projects.models import Projects
 # Create your views here.
+def project_index(request):
+    projects = Projects.objects.all() # Performing a ORM query
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
