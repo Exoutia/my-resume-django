a297666966e91e08d7bc3962ce8dbba3f4e49359
Exoutia <jhav365@gmail.com>
2023-03-13 10:26:25 +0530

    adding the project details in view of projects to show the projects details in site.

--- personal_portfolio/projects/views.py
+++ personal_portfolio/projects/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from projects.models import Projects
+
 # Create your views here.
 def project_index(request):
-    projects = Projects.objects.all() # Performing a ORM query
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
+    projects = Projects.objects.all()  # Performing a ORM query
+    context = {"projects": projects}
+    return render(request, "project_index.html", context)
+
+
+def project_detail(request, pk):
+    project = Projects.objects.get(pk=pk)
+    context = {"project": project}
+    return render(request, "project_detial.html", context)
