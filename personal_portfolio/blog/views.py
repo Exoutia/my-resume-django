8058b50541e478fcd34ebbe8dd20c0c296d8f619
Exoutia <jhav365@gmail.com>
2023-03-13 19:54:03 +0530

    adding the form to the views and adding the function to submit and save the comments

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
 
+from .forms import CommentForm
+
 def blog_index(request):
     posts = Post.objects.all().order_by('-coreated_on')
     context = {
@@ -24,10 +26,25 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author = form.cleaned_data["author"],
+                body = form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form":form,
     }
+    return render(request, "blog_detail.html", context)
 
-    
+
