655ffbab2dcfb32592e3c1aa9781e85236d693e2
Exoutia <jhav365@gmail.com>
2023-05-10 09:53:52 +0530

    making it so that bootstrap works

--- personal_portfolio/blog/forms.py
+++ personal_portfolio/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
 
 
 class CommentForm(forms.Form):
@@ -19,3 +19,22 @@ class CreateBlogForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'body', 'categories']
+
+    title = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(
+            attrs= {"class": "form-control", "placeholder": "Title of Your Blog"}
+        )
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs= {"class": "form-control"}
+        )
+    )
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects,
+        widget=forms.SelectMultiple(
+            attrs={"class": "form-control"},
+        ),
+        help_text = "Press ctrl in keyborad to select more categories."
+    )
